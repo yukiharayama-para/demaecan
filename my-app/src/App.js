e36f941e81d29eca665d5e2035575cac373098eb
yukiharayama-para <b-o-c.bbcn.zone07@outlook.jp>
2021-07-16 09:49:22 +0900

    add demaecan_logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import logo from '/my-app/src/demaecan_logo.png';
 
 
 
@@ -32,9 +33,10 @@ import MenuIcon from '@material-ui/icons/Menu';
                 <MenuIcon />
               </IconButton>
               <Typography variant="h6" className={classes.title}>
-                News
+                {logo}
               </Typography>
-              <Button color="inherit">Login</Button>
+              <Button color="inherit">新規登録</Button>
+              <Button color="inherit">ログイン</Button>
             </Toolbar>
           </AppBar>
         </div>
